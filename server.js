56c4623ae048818a85c15ac3e1a261809a4790e2
osama673 <oussemametsahel@gmail.com>
2024-01-01 18:21:26 +0100

    first

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const port = 3001; 
+
+
+// Middleware to verify the time of the request
+app.use((req, res, next) => {
+  const date = new Date();
+  const dayOfWeek = date.getDay();
+  const hours = date.getHours();
+
+  if (dayOfWeek >= 1 && dayOfWeek <= 5 && hours >= 9 && hours < 17) {
+    next(); // Proceed to the next middleware or route handler
+  } else {
+    res.send(
+      "The web application is only available during working hours (Monday to Friday, from 9 to 17)."
+    );
+  }
+});
+// Définir le dossier "public" comme dossier statique
+app.use(express.static("public"));
+
+// Route handlers
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get("/services", (req, res) => {
+  res.sendFile(__dirname + "/public/services.html");
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile(__dirname + "/public/contact.html");
+});
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
